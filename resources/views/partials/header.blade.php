279743acd6f53215fc2abf083b005d84a2ab762c
ElenaLj <elenaljubijankic@gmail.com>
2022-02-02 16:10:50 +0100

    insert header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,40 @@
-<header></header>
+<header class="header container">
+    <div class="logo">
+        <img src="{{asset("images/dc-logo.png")}}" alt="DC Comics Logo">
+    </div>
+
+    <nav class="header-nav">
+        <ul>
+            <li>
+                <a href="#">characters</a>
+            </li>
+            <li>
+                <a href="#">comics</a>
+            </li>
+            <li>
+                <a href="#">movies</a>
+            </li>
+            <li>
+                <a href="#">tv</a>
+            </li>
+            <li>
+                <a href="#">games</a>
+            </li>
+            <li>
+                <a href="#">collectibles</a>
+            </li>
+            <li>
+                <a href="#">video</a>
+            </li>
+            <li>
+                <a href="#">fans</a>
+            </li>
+            <li>
+                <a href="#">news</a>
+            </li>
+            <li>
+                <a href="#">shop</a>
+            </li>
+        </ul>
+    </nav>
+</header>
